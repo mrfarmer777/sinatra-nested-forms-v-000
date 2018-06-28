51ad2433700250391b3539045f03103bf4624ccc
Matt Farmer <mrfarmer777@gmail.com>
2018-06-28 05:16:51 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  SHIPS=[]
+  
+  def initialize(name,type,booty)
+    @name=name
+    @type=type
+    @booty=booty
+  end
+
+  def all 
+    SHIPS
+  end
+  
+  
 end
