d09bae18c54ee6fe721b8640b4374c58226fb80d
Matt Farmer <mrfarmer777@gmail.com>
2018-06-28 05:54:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,21 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get "/" do
+      erb :root
+    end
+
+    get "/new" do
+      erb :'pirates/new' #! How to route to nested views
+    end
+
+    post "/pirates" do
+      @pirate=Pirate.new(params[:pirate])
+      @ship1=Ship.new(params[:pirate][:ships][0])
+      @ship2=Ship.new(params[:pirate][:ships][1])
+
+      erb :'pirates/show'
+    end
 
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  SHIPS=[]
+
+  def initialize(ship)
+    @name=ship[:name]
+    @type=ship[:type]
+    @booty=ship[:booty]
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
+
+
 end
